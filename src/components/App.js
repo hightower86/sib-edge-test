c0c9283d2e8bded1422c918b950098ea00bc7cac
Adel <htwr86@gmail.com>
2020-02-25 23:24:29 +0300

    usersList component wigh base stylw

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+
+import UsersList from './UsersList';
 import aStyle from './app.module.scss';
 
 const App = () => {
   return (
     <div className={aStyle.container}>
-      <h1>App</h1>
+      <h1>Users list from sibEDGE</h1>
+      <UsersList />
     </div>
   );
 };
---
+++ src/components/UsersList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import uStyle from './users-list.module.scss';
+
+const UsersList = () => {
+  return <div className={uStyle.ulist}>list of users</div>;
+};
+
+export default UsersList;
